4005bffa7fb1250680d97c3cbc91d8519daea3b3
Gurupranav-tech <gurupranav08@gmail.com>
2025-02-17 18:36:44 +0000

    Fixed charts error

--- src/pages/coin.tsx
+++ src/pages/coin.tsx
@@ -140,7 +140,7 @@ export default function Coin() {
         },
       },
     };
-  }, [theme, formatter, scale, screen]);
+  }, [theme, formatter, scale, screen, coinsHistory, coinsHistory.data]);
 
   if (coinsHistory.isFetching) return <p>Loading...</p>;
 
